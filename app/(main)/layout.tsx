92fac73cc8ba370fdd73942b6458c368ed57e6b3
abey asmare <abeyasmare07@gmail.com>
2025-08-02 01:28:39 +0300

    feature: add metadata to the home page

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -7,3 +7,22 @@ export default function MainLayout({children}: {children: React.ReactNode}) {
     </div>
   )
 }
+
+
+
+export const metadata = {
+  title: "Success Academy",
+  description: "Your Shortcut to success.",
+  openGraph: {
+    title: "Success Academy",
+    description: "Your Shortcut to success.",
+    images: [
+      {
+        url: "images/success_academy-logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Success Academy Banner",
+      },
+    ],
+  },
+}
