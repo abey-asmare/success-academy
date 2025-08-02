9169f51ddb54594bd589cc6a2e2cc8f79eb5264e
abey asmare <abeyasmare07@gmail.com>
2025-08-02 14:26:45 +0300

    wip: update ui

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,8 +1,19 @@
 import { Metadata } from 'next'
 import Nav from './components/Nav'
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
 export const metadata: Metadata = {
-  title: "Success Academy",
+  title: "Home page",
+  openGraph: {
+    images: [
+      {
+        url: `${baseUrl}/opengraph-image.png`,
+        width: 1024,
+        height: 1024,
+      },
+    ],
+  },
 }
 
 export default function MainLayout({children}: {children: React.ReactNode}) {
