f6859cdac5938e570f640d5ed27acdb93ad99ef8
abey asmare <abeyasmare07@gmail.com>
2025-08-12 23:28:20 +0300

    Revert "bug-fix: cors error"
    This reverts commit 370ad892838a458d92f4c4b794dc8247ca3114d4.

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,10 +46,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-     proxyUrl="/__clerk"
-  publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider>
       <html lang="en" className="scroll-smooth md:scroll-auto">
         <meta property="og:title" content="Success Academy" />
         <meta property="og:description" content="Your Shortcut to success." />
