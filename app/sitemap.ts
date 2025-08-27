7a9aa4ca1b6b98269be7e18b2ae859c12ba6202a
abey asmare <abeyasmare07@gmail.com>
2025-08-27 23:25:23 +0300

    topo: fix typo in robots and sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,11 +3,11 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://success-academy.et',
+      url: 'https://successacademy.et',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
-      images: ['https://success-academy.et/images/success_academy-logo.png'],
+      images: ['https://successacademy.et/images/success_academy-logo.png'],
     },
 
   ]
