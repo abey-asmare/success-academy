a8f1ac125f88e27906b2a825c08f4c9085195dba
abey asmare <abeyasmare07@gmail.com>
2025-08-27 23:54:02 +0300

    metadata: remove privacy and terms of use from crawlling

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/dashboard/*'],
+      disallow: ['/dashboard/*', 'privacy-policy', 'terms-of-use'],
     },
     sitemap: 'https://successacademy.et/sitemap.xml',
   }
